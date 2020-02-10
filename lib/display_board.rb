f6fc938e548bff14c4b86b99b19950a1a0e21599
Felix Rodriguez <frod25@gmail.com>
2020-02-10 06:30:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,7 +7,3 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts "-----------"
   puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
-
-display_board
-
-display_board(["", "o", "x", "o", "x", "o", "x", "o", "x"])
